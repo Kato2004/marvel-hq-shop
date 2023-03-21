13221c38f20eef3d6af5a8c3ea9afa56fe69f6c7
Kato2004 <kauaalvesnascimento22@gmail.com>
2023-03-21 14:15:08 -0300

    project started: starting the project with basic settings besides eslint for error checking and prettier for formatting

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { AllRoutes } from "./core/routes";
+import { GlobalStyle } from "./styles/global-styles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <AllRoutes />
+    </>
+  );
+}
+
+export default App;
